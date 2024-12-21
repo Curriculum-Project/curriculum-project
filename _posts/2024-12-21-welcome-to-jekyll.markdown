7c0b0efb4c97f840f225a0a56c25fd40d42ebc5f
Leonardo Gomes <leonardodasigomes@gmail.com>
2024-12-21 15:57:07 -0300

    Adjust script to create file with date

--- _posts/2024-12-21-welcome-to-jekyll.markdown
+++ _posts/2024-12-21-welcome-to-jekyll.markdown
@@ -1,7 +1,8 @@
 ---
 layout: post
 title:  "Welcome to Jekyll!"
-date:   2024-12-21 15:31:05 -0300
+date:   2024-10-21T16:39:45Z
+author: LeoSilvaGomes
 categories: jekyll update
 ---
 You’ll find this post in your `_posts` directory. Go ahead and edit it and re-build the site to see your changes. You can rebuild the site in many different ways, but the most common way is to run `jekyll serve`, which launches a web server and auto-regenerates your site when a file is updated.
--- scripts/create_post.sh
+++ scripts/create_post.sh
@@ -1,5 +1,6 @@
 path_post="_posts/"
-file_name="post-$GITHUB_ISSUE_NUMBER.md"
+file_date=$GITHUB_ISSUE_CREATED_AT | -c1-10
+file_name="{$file_date}-post.md"
 file_path="${path_post}${file_name}"
 
 echo "---" >> ${file_path}
@@ -7,7 +8,7 @@ echo "layout: post" >> ${file_path}
 echo "title: $GITHUB_ISSUE_TITLE" >> ${file_path}
 echo "date: $GITHUB_ISSUE_CREATED_AT" >> ${file_path}
 echo "author: $GITHUB_ISSUE_USER" >> ${file_path}
-echo "url: $GITHUB_ISSUE_URL" >> ${file_path} 
+# echo "url: $GITHUB_ISSUE_URL" >> ${file_path} 
 echo "---" >> ${file_path}
 echo "$GITHUB_ISSUE_BODY" >> ${file_path}
 
