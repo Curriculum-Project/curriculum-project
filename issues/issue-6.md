d9b73d32f2eee617dc992acf5b5e8869b7e935ee
Leonardo Gomes <leonardodasigomes@gmail.com>
2024-12-21 14:25:32 -0300

    adjust tab to a file

--- issues/issue-6.md
+++
@@ -1,7 +0,0 @@
-# eu sou um teste 
-
-## Descrição
-Testando 
-
-## Autor: Gabrielsoac
-Issue URL: [https://github.com/Curriculum-Project/curriculum-project/issues/6](https://github.com/Curriculum-Project/curriculum-project/issues/6)
--- scripts/create_post.sh
+++ scripts/create_post.sh
@@ -5,12 +5,13 @@ file_path="${path_post}${file_name}"
 echo "created: $GITHUB_ISSUE_CREATED_AT" >> ${file_path}
 echo "" >> ${file_path}
 
-echo "title: $GITHUB_ISSUE_TITLE" >> ${file_path}
+echo "title: $GITHUB_ISSUE_TITLE\n" >> ${file_path}
 echo "" >> ${file_path}
 
 echo "description: |" >> ${file_path}
-echo "    $GITHUB_ISSUE_BODY" >> ${file_path}
-echo "" >> ${file_path}
+while IFS= read -r line; do
+  echo "    $line" >> ${file_path}
+done <<< "$GITHUB_ISSUE_BODY"
 
 echo "author: $GITHUB_ISSUE_USER" >> ${file_path}
 echo "issue_url: $GITHUB_ISSUE_URL" >> ${file_path} 
