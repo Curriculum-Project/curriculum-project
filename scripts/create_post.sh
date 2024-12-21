8eaebeddc975ea11d335ee4f49e90da3553012ec
Leonardo Gomes <leonardodasigomes@gmail.com>
2024-12-21 14:07:03 -0300

    Create env files

--- scripts/create_post.sh
+++ scripts/create_post.sh
@@ -1,16 +1,18 @@
 path_post="posts/"
-file_name="post-${ github.event.issue.number }.yaml"
+file_name="post-$GITHUB_ISSUE_NUMBER.yaml"
 file_path="${path_post}${file_name}"
 
-echo "created: ${{ github.event.issue.created_at }}" >> ${file_path}
+ls
+
+echo "created: $GITHUB_ISSUE_CREATED_AT" >> ${file_path}
 echo "" >> ${file_path}
 
-echo "title:" > ${!file_name}
+echo "title: $GITHUB_ISSUE_TITLE" >> ${file_path}
 echo "" >> ${file_path}
 
 echo "description: |" >> ${file_path}
-echo "    ${{ github.event.issue.body }}" >> ${file_path}
+echo "    $GITHUB_ISSUE_BODY" >> ${file_path}
 echo "" >> ${file_path}
 
-echo "author: ${{ github.event.issue.user.login }}" >> ${file_path}
-echo "issue_url: ${{ github.event.issue.html_url }}" >> ${file_path} 
+echo "author: $GITHUB_ISSUE_USER" >> ${file_path}
+echo "issue_url: $GITHUB_ISSUE_URL" >> ${file_path} 
