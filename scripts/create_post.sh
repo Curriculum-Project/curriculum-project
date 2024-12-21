635fbf8b66b21cbf783ab62aca8bd1b2375bb826
Leonardo Gomes <leonardodasigomes@gmail.com>
2024-12-21 13:58:45 -0300

    Adjust sh script

--- scripts/create_post.sh
+++ scripts/create_post.sh
@@ -1,16 +1,16 @@
 path_post="posts/"
-file_name="post-${{ github.event.issue.number }}.yaml"
+file_name="post-${ github.event.issue.number }.yaml"
 file_path="${path_post}${file_name}"
 
-echo "created: ${{ github.event.issue.created_at }}" >> ${!file_path}
-echo "" >> ${!file_path}
+echo "created: ${{ github.event.issue.created_at }}" >> ${file_path}
+echo "" >> ${file_path}
 
 echo "title:" > ${!file_name}
-echo "" >> ${!file_path}
+echo "" >> ${file_path}
 
-echo "description: |" >> ${!file_path}
-echo "    ${{ github.event.issue.body }}" >> ${!file_path}
-echo "" >> ${!file_path}
+echo "description: |" >> ${file_path}
+echo "    ${{ github.event.issue.body }}" >> ${file_path}
+echo "" >> ${file_path}
 
-echo "author: ${{ github.event.issue.user.login }}" >> ${!file_path}
-echo "issue_url: ${{ github.event.issue.html_url }}" >> ${!file_path} 
+echo "author: ${{ github.event.issue.user.login }}" >> ${file_path}
+echo "issue_url: ${{ github.event.issue.html_url }}" >> ${file_path} 
