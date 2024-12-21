49812e35b12209bbe4fd5ed2de43a8e200be3f9b
Leonardo Gomes <leonardodasigomes@gmail.com>
2024-12-21 11:06:38 -0300

    Adjust create post script

--- scripts/create_post.sh
+++ scripts/create_post.sh
@@ -1,7 +1,13 @@
-echo "# ${{ github.event.issue.title }}" > issues/issue-${{ github.event.issue.number }}.md
-echo "" >> issues/issue-${{ github.event.issue.number }}.md
-echo "## Descrição" >> issues/issue-${{ github.event.issue.number }}.md
-echo "${{ github.event.issue.body }}" >> issues/issue-${{ github.event.issue.number }}.md
-echo "" >> issues/issue-${{ github.event.issue.number }}.md
-echo "## Autor: ${{ github.event.issue.user.login }}" >> issues/issue-${{ github.event.issue.number }}.md
-echo "Issue URL: [${{ github.event.issue.html_url }}](${{ github.event.issue.html_url }})" >> issues/issue-${{ github.event.issue.number }}.md
+path_post=posts/
+file_name=${{ github.event.issue.number }}
+file_path=${!path_post}${!file_name}.md
+
+echo "# ${{ github.event.issue.title }}" > ${!file_path}
+echo "" >> ${!file_path}
+
+echo "## Descrição" >> ${!file_path}
+echo "${{ github.event.issue.body }}" >> ${!file_path}
+echo "" >> ${!file_path}
+
+echo "## Autor: ${{ github.event.issue.user.login }}" >> ${!file_path}
+echo "Issue URL: [${{ github.event.issue.html_url }}](${{ github.event.issue.html_url }})" >> ${!file_path}
