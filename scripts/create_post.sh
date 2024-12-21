4d7e109e256e96a965a77d3ec25b321909cd4cb2
Leonardo Gomes <leonardodasigomes@gmail.com>
2024-12-21 15:21:09 -0300

    Add jekyll

--- scripts/create_post.sh
+++ scripts/create_post.sh
@@ -1,17 +1,13 @@
-path_post="posts/"
-file_name="post-$GITHUB_ISSUE_NUMBER.yaml"
+path_post="page/_posts/"
+file_name="post-$GITHUB_ISSUE_NUMBER.md"
 file_path="${path_post}${file_name}"
 
-echo "created: $GITHUB_ISSUE_CREATED_AT" >> ${file_path}
-echo "" >> ${file_path}
-
-echo "title: $GITHUB_ISSUE_TITLE\n" >> ${file_path}
-echo "" >> ${file_path}
-
-echo "description: |" >> ${file_path}
-while IFS= read -r line; do
-  echo "    $line" >> ${file_path}
-done <<< "$GITHUB_ISSUE_BODY"
-
+echo "---" >> ${file_path}
+echo "layout: post" >> ${file_path}
+echo "title: $GITHUB_ISSUE_TITLE" >> ${file_path}
+echo "date: $GITHUB_ISSUE_CREATED_AT" >> ${file_path}
 echo "author: $GITHUB_ISSUE_USER" >> ${file_path}
-echo "issue_url: $GITHUB_ISSUE_URL" >> ${file_path} 
+echo "url: $GITHUB_ISSUE_URL" >> ${file_path} 
+echo "---" >> ${file_path}
+echo "$GITHUB_ISSUE_BODY" >> ${file_path}
+
