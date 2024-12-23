bc84ab75131d0c0fb10f264aa26eaa5461faec1d
Leonardo Gomes <leonardodasigomes@gmail.com>
2024-12-23 15:41:05 -0300

    Create visualization to about and experience

--- scripts/create_experiencia_profissional.sh
+++ scripts/create_experiencia_profissional.sh
@@ -1,8 +1,9 @@
 path_post="_profissional-experience/"
 file_date="$GITHUB_ISSUE_CREATED_AT"
-file_name="${file_date:0:10}-experience-$GITHUB_ISSUE_NUMBER.yaml"
+file_name="${file_date:0:10}-experience-$GITHUB_ISSUE_NUMBER.md"
 file_path="${path_post}${file_name}"
 
+echo "---" >> ${file_path}
 echo "created: $GITHUB_ISSUE_CREATED_AT" >> ${file_path}
 echo "" >> ${file_path}
 
@@ -10,3 +11,4 @@ echo "nome_empresa: $GITHUB_ISSUE_TITLE" >> ${file_path}
 echo "" >> ${file_path}
 
 echo "$GITHUB_ISSUE_BODY"  >> ${file_path}
+echo "---" >> ${file_path}
