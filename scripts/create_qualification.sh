a4764c8d536bdc1c19a132095c78e2292d764fb4
Leonardo Gomes <leonardodasigomes@gmail.com>
2024-12-26 14:04:22 -0300

    Create wualification workflow

---
+++ scripts/create_qualification.sh
@@ -0,0 +1,16 @@
+path_post="_qualification/"
+file_date="$GITHUB_ISSUE_CREATED_AT"
+file_name="${file_date:0:10}-qualification-$GITHUB_ISSUE_NUMBER.md"
+file_path="${path_post}${file_name}"
+
+echo "---" >> ${file_path}
+echo "created: $GITHUB_ISSUE_CREATED_AT" >> ${file_path}
+echo "author: $GITHUB_ISSUE_USER" >> ${file_path}
+echo "" >> ${file_path}
+
+echo "nome: $GITHUB_ISSUE_TITLE" >> ${file_path}
+echo "" >> ${file_path}
+
+echo "$GITHUB_ISSUE_BODY" | egrep -o '^[^---]+' >> ${file_path}
+echo "---" >> ${file_path}
+echo "$GITHUB_ISSUE_BODY" | sed 's/.*[http>] *//' | grep -oP '(?<=\().*?(?=\))'  >> ${file_path}
