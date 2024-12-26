3324c02487656a51e885d837154f3d8c05b75fc7
Leonardo Gomes <61520601+LeoSilvaGomes@users.noreply.github.com>
2024-12-26 17:21:59 -0300

    Update create_qualification.sh

--- scripts/create_qualification.sh
+++ scripts/create_qualification.sh
@@ -1,6 +1,6 @@
 path_post="_qualification/"
 file_date="$GITHUB_ISSUE_CREATED_AT"
-file_name="${file_date:0:10}-qualification-$GITHUB_ISSUE_NUMBER.md"
+file_name="${file_date:0:10}-qual-$GITHUB_ISSUE_NUMBER.md"
 file_path="${path_post}${file_name}"
 
 echo "---" >> ${file_path}
@@ -11,6 +11,6 @@ echo "" >> ${file_path}
 echo "nome: $GITHUB_ISSUE_TITLE" >> ${file_path}
 echo "" >> ${file_path}
 
-echo "$GITHUB_ISSUE_BODY" | egrep -ws '(---.*)' -B10 >> ${file_path}
+echo "$GITHUB_ISSUE_BODY" | egrep -ws '(---.*)' -B20 >> ${file_path}
 echo "" >> ${file_path}
 echo "$GITHUB_ISSUE_BODY" | egrep -ws '(---.*)' -A10 | grep -oP '(?<=\().*(?=\))' >> ${file_path}
